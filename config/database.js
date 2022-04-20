7ecce0a5a4c6f7c56e352f6caeec0fb91947b0c2
luizalinda <84139409+luizalinda@users.noreply.github.com>
2022-04-20 10:57:08 -0300

    Add files via upload

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const conexao = async() => {
-    var atlas = await mongoose.connect('-')
+    var atlas = await mongoose.connect('mongodb+srv://userAdmin:Lu0612@fiaptecnico.0ptot.mongodb.net/test')
 }
 
 module.exports = conexao
